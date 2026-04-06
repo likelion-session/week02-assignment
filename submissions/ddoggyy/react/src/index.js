28615914db41978db8ded4185cb72f5c19194cee
Sangmin Lee <do_ggy@naver.com>
2026-04-06 20:26:15 +0900

    added practice2 project

--- submissions/ddoggyy/react/src/index.js
+++ submissions/ddoggyy/react/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Counter from './Counter'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Counter />
 );
